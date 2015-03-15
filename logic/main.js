09516a7f975575f841eb5c909e87195dbb0b1705
Greg Marshall <greg@marshallworks.com>
2015-03-15 10:53:50 -0400

    draw basic board

--- logic/main.js
+++ logic/main.js
@@ -6,7 +6,7 @@
 	var UT = window.UT || {};
 
 	// SCOPE:
-	var init, generateBoard;
+	var init, generateBoard, drawBoard;
 	// Base Board Settings
 	var boardTileWidth = 10;
 	var boardTileHeight = 10;
@@ -41,11 +41,39 @@
 		return board;
 	};
 
+	drawBoard = function (board) {
+		var _i, _j, height, width;
+		if (board == null) {
+			board = generateBoard();
+		}
+		var tilePxWidth = 28;
+		var tilePxHeight = 28;
+		var canvas = UT.qs('#board');
+		if (canvas.getContext) {
+			var ctx = canvas.getContext('2d');
+			ctx.fillStyle = 'rgb(0, 200, 0)';
+			// Row loop
+			for(_i = 0, height = board.length; _i < height; _i++) {
+				// Tile loop
+				for(_j = 0, width = board[_i].length; _j < width; _j++) {
+					// Draw tile
+					ctx.fillRect(
+						_j * (tilePxWidth + 2),
+						_i * (tilePxHeight + 2),
+						tilePxWidth,
+						tilePxHeight);
+				}
+			}
+		} else {
+			console.log('Canvas context not supported.');
+		}
+		return board;
+	};
+
 	// Initializer
 	init = function () {
 		console.log('Page Load');
-		var board = generateBoard();
-		console.log(board);
+		drawBoard();
 		return true;
 	};
 
