71b472d96407af9b467dc25a4ed7d335af689ad5
Greg Marshall <greg@marshallworks.com>
2015-03-15 12:14:38 -0400

    abstract out sim state

--- logic/main.js
+++ logic/main.js
@@ -8,20 +8,46 @@
 	// SCOPE:
 	var init, generateBoard, drawBoard;
 	// Base Board Settings
-	var boardTileWidth = 10;
-	var boardTileHeight = 10;
+	var boardDefaultWidth = 10;
+	var boardDefaultHeight = 10;
 	var tileDirections = ['up', 'right', 'down', 'left'];
+	var currentState = null;
 
-	// Functions
+	// Simulation State
+	function SimState (boardWidth, boardHeight, startX, startY) {
+		// Set defaults
+		if (boardWidth == null) {
+			boardWidth = boardDefaultWidth;
+		}
+		if (boardHeight == null) {
+			boardHeight = boardDefaultHeight;
+		}
+		if (startX == null) {
+			startX = Math.floor(Math.random() * boardWidth);
+		}
+		if (startY == null) {
+			startY = Math.floor(Math.random() * boardHeight);
+		}
+		if (startX < 0 || startY < 0 || startX > boardWidth - 1 || startY > boardHeight - 1) {
+			console.log('Board Starting Position was out of bounds: x->' + startX + ' y->' + startY);
+			startX = 0;
+			startY = 0;
+		}
+		this.board = generateBoard(boardWidth, boardHeight);
+		this.board[startY][startX].visited = true;
+		this.position = {
+			x: startX,
+			y: startY
+		};
+		this.size = {
+			width: boardWidth,
+			height: boardHeight
+		};
+		return this;
+	}
+
 	generateBoard = function (tileWidth, tileHeight) {
-		var _i, _j, startPos;
-		// Board defaults
-		if (tileWidth == null) {
-			tileWidth = boardTileWidth;
-		}
-		if (tileHeight == null) {
-			tileHeight = boardTileHeight;
-		}
+		var _i, _j;
 		// Create board
 		var board = [];
 		// Loop to create tile rows.
@@ -35,22 +61,15 @@
 					// Math.random() is 0 inclusive and 1 exclusive; so using Math.floor for an even
 					// distribution of directions on the board.
 					direction: tileDirections[Math.floor(Math.random() * tileDirections.length)],
-					occupied: false,
 					visited: false
 				};
 			}
 		}
-		startPos = board[Math.floor(Math.random() * tileHeight)][Math.floor(Math.random() * tileWidth)];
-		startPos.occupied = true;
-		startPos.visited = true;
 		return board;
 	};
 
-	drawBoard = function (board) {
-		var _i, _j, height, width;
-		if (board == null) {
-			board = generateBoard();
-		}
+	drawBoard = function (sim) {
+		var _i, _j;
 		var tilePxWidth = 28;
 		var tilePxHeight = 28;
 		var tilePadding = 6;
@@ -58,20 +77,20 @@
 		var occupiedColor = 'rgb(170, 0, 0)';
 		var visiteColor = 'rgb(120, 120, 120)';
 		var canvas = UT.qs('#board');
-		canvas.width = tilePxWidth * tilePadding * board.length;
-		canvas.height = tilePxHeight * tilePadding * board[0].length;
+		canvas.width = tilePxWidth * tilePadding * sim.size.width;
+		canvas.height = tilePxHeight * tilePadding * sim.size.height;
 		if (canvas.getContext) {
 			var ctx = canvas.getContext('2d');
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			// Row loop
-			for(_i = 0, height = board.length; _i < height; _i++) {
+			for(_i = 0; _i < sim.size.height; _i++) {
 				// Tile loop
-				for(_j = 0, width = board[_i].length; _j < width; _j++) {
+				for(_j = 0; _j < sim.size.width; _j++) {
 					// Draw tile
 					ctx.fillStyle = defaultColor;
-					if (board[_i][_j].occupied) {
+					if (_i === sim.position.y && _j === sim.position.x) {
 						ctx.fillStyle = occupiedColor;
-					} else if (board[_i][_j].visited) {
+					} else if (sim.board[_i][_j].visited) {
 						ctx.fillStyle = visiteColor;
 					}
 					ctx.fillRect(
@@ -84,13 +103,14 @@
 		} else {
 			console.log('Canvas context not supported.');
 		}
-		return board;
+		return sim;
 	};
 
 	// Initializer
 	init = function () {
 		console.log('Page Load');
-		drawBoard();
+		currentState = new SimState();
+		drawBoard(currentState);
 		return true;
 	};
 
